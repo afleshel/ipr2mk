ab8e9728c03281c94a4235f17165b72a455a7687
Nat Pryce <sw@natpryce.com>
2014-03-05 15:13:54 +0000

    Fix JARFLAGS and change name of classes directory

--- java.mk
+++ java.mk
@@ -6,6 +6,6 @@ JAVAC ?= javac
 java_classpath_of=$(subst $(eval) ,:,$(wildcard $1))
 
 %.jar:
-	@mkdir -p $@-content/
-	$(JAVAC) $(filter %.java,$^) -cp $(call java_classpath_of,$(filter %.jar,$^)) -d $@-content/
-	$(JAR) $(JARFLAGS) -cf $ $@ -C $@-content/ .
+	@mkdir -p $(basename $@)/
+	$(JAVAC) -cp $(call java_classpath_of,$(filter %.jar,$^)) -d $(basename $@)/ $(filter %.java,$^)
+	$(JAR) -cf$(JARFLAGS) $@ -C $(basename $@)/ .
